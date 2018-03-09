eba99d4abf524da7bf32648ae820b1cae9b1acff
Gabriel Franca <gabriel.franca@gmail.com>
2018-03-09 09:42:41 -0300

    configurando o login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -8,6 +8,8 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=> true,
+		"footer"=> true,
 		"data"=>[]
 	];
 
@@ -27,7 +29,7 @@ class Page {
 
 	$this->setData($this->options["data"]);
 
-	$this->tpl->draw("header");
+	if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 	private function setData($data = array())
@@ -49,7 +51,7 @@ class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 
 	}
